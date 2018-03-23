928fa37fc680efa90f27242006242c9ed9041a2a
Alex <alexliu0809@hotmail.com>
2018-03-23 15:30:08 -0500

    Update Password Policy

--- hcre.c
+++ hcre.c
@@ -4,7 +4,6 @@
 #include <locale.h>
 #include <string.h>
 #include "rules.h"
-
 //#define DEBUG
 
 typedef struct RuleHash {
@@ -37,14 +36,155 @@ void usage(char *hcre) {
     printf("\n");
 }
 
+int check_length = -1;
+int check_digit = 0;
+int check_letter = 0;
+int check_lower = 0;
+int check_upper = 0;
+
+static int check_password_policy(char *pw_buf, const int pw_len)
+{
+  /*
+  printf("%d\n", check_length);
+  printf("%d\n", check_digit);
+  printf("%d\n", check_lower);
+  printf("%d\n", check_letter);
+  printf("%d\n", check_upper);
+  printf("%d\n", check_symbol);
+  printf("The End\n");
+  */
+  if (check_length != -1 && check_length >= 0)
+  {
+    //if pw_len < check_length, reject
+    if (pw_len < check_length)
+    {
+      return 0;
+    }
+  }
+
+
+
+  int digit_flag = check_digit != 0? 1:0;
+  int lower_flag = check_lower != 0? 1:0;
+  int letter_flag = check_letter != 0? 1:0;
+  int upper_flag = check_upper != 0? 1:0;
+  int flag_count = digit_flag + lower_flag + letter_flag + upper_flag;
+  //symbol_flag = check_symbol != 0? 1:0;
+
+  for (int i = 0; i < pw_len; i ++)
+  {
+    /* PRINTABLE ASCII CHECK 
+    //Not need this: Because input word and search word are both printable chars.
+    //Valid search result will only contain 1 \t
+    if ((pw_buf[i]) < 32 || (pw_buf[i]) > 126)
+    {
+      return 0;
+    }
+    */
+
+    if (flag_count == 0)
+    {
+      return 1;
+    }
+
+    /* Rejection Policy Check */
+    // If contains digit, 
+    if ( ( digit_flag == 1 ) && (pw_buf[i]) >= '0' && (pw_buf[i]) <= '9')
+    {
+        digit_flag = 0; // satisfy digit flag
+        flag_count -= 1;
+    }
+    if ( (lower_flag == 1) && (pw_buf[i]) >= 'a' && (pw_buf[i]) <= 'z')
+    {
+        lower_flag = 0; // satisfy lower_flag
+        flag_count -= 1;
+    }
+    if ( (upper_flag == 1) && (pw_buf[i]) >= 'A' && (pw_buf[i]) <= 'Z')
+    {
+        upper_flag = 0; // satisfy upper_flag
+        flag_count -=1;
+    }
+    if ( (letter_flag == 1) && (((pw_buf[i]) >= 'a' && (pw_buf[i]) <= 'z') || ((pw_buf[i]) >= 'A' && (pw_buf[i]) <= 'Z')) )
+    {
+        letter_flag = 0; // satisfy letter_flag
+        flag_count -= 1;
+    }
+    /*
+    if ( *(pw_buf[i] >= 'A' && *(pw_buf[i]) <= 'Z')
+    {
+      if (symbol_flag == 1)
+      {
+        printf("symbol_flag Not Implemented");
+      }
+    }
+    */
+  }
+  if (flag_count > 0)
+  {
+    return 0;
+  }
+
+  return 1;
+}
+
+//int check_symbol = 0;
+bool prefix(const char *pre, const char *str)
+{
+    return strncmp(pre, str, strlen(pre)) == 0;
+}
 
 int main(int argc, char **argv) {
+    int new_argc = argc;
+  for (int i = 0; i < argc; i++)
+  {
+    //printf("%s\n",argv[i]);
+    if (prefix("--length=",argv[i]))
+    {
+      new_argc --;
+      check_length = atoi(argv[i]+9);
+    }
+    else if (prefix("--digit",argv[i]))
+    {
+      new_argc --;
+      check_digit = 1;
+    }
+    else if (prefix("--upper",argv[i]))
+    {
+      new_argc --;
+      check_upper = 1;
+    }
+    else if (prefix("--lower",argv[i]))
+    {
+      new_argc --;
+      check_lower = 1;
+    }
+    //else if (prefix("--symbol",argv[i]))
+    //{
+      //new_argc --;
+      //check_symbol = 1;
+    //}
+    else if (prefix("--letter",argv[i]))
+    {
+      new_argc --;
+      check_letter = 1;
+    }
+
+  }
+
+    argc = new_argc;
     // argv[0] is name
     if (argc < 5) { usage(argv[0]); return 0; }
 
 
     /* ARGS Check */
-
+    if (prefix("-r",argv[2]) == 0)
+    {
+        usage(argv[0]); return 0;
+    }
+    if (prefix("--stdout",argv[4]) == 0)
+    {
+        usage(argv[0]); return 0;
+    }
 
 
 
@@ -236,16 +376,23 @@ int main(int argc, char **argv) {
 
             }
             else{
+
+
                 #ifdef DEBUG
                 printf("%s\n", "Inside");
                 #endif
-                //resultant word
-                rule_output[rule_rtn++] = '\t';
-                fwrite(rule_output, rule_rtn, 1, stdout);
 
-                //original word
-                fwrite(line, line_len, 1, stdout);
-                fputc('\n', stdout);
+                if (check_password_policy(rule_output, rule_rtn) == 1)
+                {
+                    //resultant word
+                    rule_output[rule_rtn++] = '\t';
+                    fwrite(rule_output, rule_rtn, 1, stdout);
+
+                    //original word
+                    fwrite(line, line_len, 1, stdout);
+                    fputc('\n', stdout);
+                }
+
             }
 
             //Next hash
